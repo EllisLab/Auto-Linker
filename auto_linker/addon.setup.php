fd238a21376891a997ae514375dfdeac114c9178
Derek Jones <derek.jones@ellislab.com>
2016-02-15 06:59:19 -0800

    bumping version and travis build

--- auto_linker/addon.setup.php
+++ auto_linker/addon.setup.php
@@ -5,7 +5,7 @@ return array(
 	'author_url'  => 'https://ellislab.com/',
 	'name'        => 'Auto Linker',
 	'description' => 'Automatically creates links from URLs and/or email addresses contained within the given text.',
-	'version'     => '3.0.0',
+	'version'     => '3.0.1',
 	'namespace'   => 'User\Addons\AutoLinker',
 	'settings_exist' => FALSE
 );
