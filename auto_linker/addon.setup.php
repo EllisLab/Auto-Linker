3aef7bcee9d09e0482f38681209b6dd54e3266ac
andy <andy@packettide.com>
2021-11-19 16:25:07 -0500

    updated license, added icon

--- auto_linker/addon.setup.php
+++ auto_linker/addon.setup.php
@@ -1,11 +1,11 @@
 <?php
 
 return array(
-	'author'      => 'EllisLab',
-	'author_url'  => 'https://ellislab.com/',
+	'author'      => 'Packet Tide',
+	'author_url'  => 'https://packettide.com/',
 	'name'        => 'Auto Linker',
 	'description' => 'Automatically creates links from URLs and/or email addresses contained within the given text.',
-	'version'     => '3.0.1',
+	'version'     => '3.0.2',
 	'namespace'   => 'User\Addons\AutoLinker',
 	'settings_exist' => FALSE
 );
